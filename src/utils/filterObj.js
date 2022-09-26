d87764d85df5d8132eaf3bca108cb98b9fed17a1
Mauricio Tamez <vitxlssreturn@gmail.com>
2022-09-26 04:22:09 -0500

    chore: Add filterObj function to our project.
    This function allows us to only permit certain fields to be updated by
    the user.

---
+++ src/utils/filterObj.js
@@ -0,0 +1,13 @@
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {}
+
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el]
+    }
+  })
+
+  return newObj
+}
+
+module.exports = { filterObj }
