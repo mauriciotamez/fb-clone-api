b7128cee55bcc15d0fd822d2ad4320a54269ef92
Mauricio Tamez <vitxlssreturn@gmail.com>
2022-09-28 02:54:01 -0500

    test(users): added update and delete crud operations tests, added falso.
    We added update and delete tests, made a function to generate a random
    payload using @ngneat/falso library, the tests are running correctly
    with the use of this function, also we are now using our global instance
    of PrismaClient.

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,8 +1,7 @@
 const request = require('supertest')
 const { app } = require('../../app')
-const { PrismaClient } = require('@prisma/client')
-
-const prisma = new PrismaClient()
+const prisma = require('../prisma/prismaClient')
+const { randEmail, randFullName } = require('@ngneat/falso')
 
 /* This is our prisma function that sends the query to delete all records on the DB */
 const deleteDbEntries = async () => {
@@ -10,16 +9,48 @@ const deleteDbEntries = async () => {
   await prisma.$transaction([deleteUsersTable])
 }
 
+const generateRandomPayload = () => {
+  const payload = {
+    name: randFullName(),
+    email: randEmail(),
+    password: '123456',
+    birthday: '2000/01/01',
+    address: 'Test',
+    jobTitle: 'Full Stack Web Developer'
+  }
+  return payload
+}
+
 describe('POST /users', () => {
   describe('given a name, password, email, birthday and address', () => {
-    test('should respond with a 200 status code', async () => {
-      const response = await request(app).post('/api/v1/users').send({
-        name: 'Test data',
-        password: 'testdata',
-        email: 'testdata@test.com',
-        birthday: '2000/01/01',
-        address: 'test'
+    test('should create a user and respond with a 200 status code', async () => {
+      const response = await request(app)
+        .post('/api/v1/users')
+        .send(generateRandomPayload())
+      expect(response.statusCode).toBe(200)
+      expect(response.body.data.user).toHaveProperty('name')
+      expect(response.body.data.user.password).toBeFalsy()
+    })
+  })
+
+  describe('given a name to update, or address, email', () => {
+    test('should update the user and respond with a 204', async () => {
+      const user = await prisma.users.create({ data: generateRandomPayload() })
+      const response = await request(app)
+        .patch(`/api/v1/users/${user.id}`)
+        .send({
+          name: 'updated data'
+        })
+      expect(response.statusCode).toBe(204)
+    })
+  })
+
+  describe('given an id', () => {
+    test('should delete the user with given id, update their status field to deleted and respond with a 200', async () => {
+      const user = await prisma.users.create({
+        data: generateRandomPayload()
       })
+      const response = await request(app).delete(`/api/v1/users/${user.id}`)
       expect(response.statusCode).toBe(200)
     })
   })
